06836635fea0ddd57bfb2d361a6a434d27927d6d
Sean Mitchell <seanmitchell09022000@gmail.com>
2026-08-03 12:44:30 +0100

    update

---
+++ data.js
@@ -0,0 +1,147 @@
+const CARD_DATA = {
+
+  "corporate-objectives": [
+
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+  ],
+
+  "shareholder-wealth-and-company-valuation": [
+
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+  ],
+
+  "cost-of-equity": [
+
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+  ],
+
+  "cost-of-debt": [
+
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+  ],
+
+  "theories-of-capital-structure": [
+
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+  ],
+
+  "financing-short-and-long-term": [
+
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+  ],
+
+  "capital-investment": [
+
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+  ],
+
+  "managing-foreign-currency-risk": [
+
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+  ],
+
+  "dividend-policy": [
+
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+    {
+      question: "",
+      answer: ""
+    },
+  ]  
+
+};
