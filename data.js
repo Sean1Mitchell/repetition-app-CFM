4e23d36f19dfc727594defd951f928835bc9c849
Sean Mitchell <seanmitchell09022000@gmail.com>
2026-08-03 15:05:01 +0100

    update

--- data.js
+++ data.js
@@ -3,21 +3,109 @@ const CARD_DATA = {
   "corporate-objectives": [
 
     {
-      question: "",
-      answer: ""
+      question: "what is corporate objectives?",
+      answer: "the goals set by the management of an organisation that align with its shareholders."
     },
     {
-      question: "",
-      answer: ""
+      question: "what is Total Shareholder Return (TSR)?",
+      answer: "a measure of return for shareholders, combines capital gains and dividends received."
     },
     {
-      question: "",
-      answer: ""
+      question: "what is agency theory?",
+      answer: "the theory that their can be conflict between management and shareholders."
     },
+    {
+      question: "what are the two ways of dealing with the agency problem?",
+      answer: "internal and external mechanisms."
+    },
+    {
+      question: "what are internal mechanisms?",
+      answer: "fear of: hostile takeover, competition, job prospects, external auditors."
+    },
+    {
+      question: "what are external mechanisms?",
+      answer: "remunerations, board of directors, internal auditors, job prospects."
+    },
+    {
+      question: "what is Stock Market Hypothesis (SMH)?",
+      answer: "the ways in which to measure the performance of the stock market; operational, allocational, pricing."
+    },
+    {
+      question: "what is operational?",
+      answer: "the cost, speed, and reliability of transactions."
+    },
+    {
+      question: "what is allocational?",
+      answer: "the allocation of funds to where they can generate growth."
+    },
+    {
+      question: "what is pricing?",
+      answer: "the risk-adjusted return to investors."
+    },
+    {
+      question: "what is Efficient Market Hypothesis (EMH)?",
+      answer: "the theory that markets are rapidly relfective of all available information."
+    },
+    {
+      question: "what is weak?",
+      answer: "the market is only reflective of historical information."
+    },
+    {
+      question: "what is semi-strong?",
+      answer: "the market is reflective of historic and public disclosred information."
+    },
+    {
+      question: "what is strong?",
+      answer: "the market is reflective of historic, public disclosed, and private information (insider trading)."
+    }
   ],
 
   "shareholder-wealth-and-company-valuation": [
 
+    {
+      question: "How do we increase shareholder wealth?",
+      answer: "add value: capital invested x (actual return - required return). Planning horizon is how long we can make it happen."
+    },
+    {
+      question: "what are the three things that increase wealth?",
+      answer: "return on existing capital, investment in positive spreads, and planning horizon."
+    },
+    {
+      question: "what are the two things that decrease wealth?",
+      answer: "required rate of return and investment in negative spreads."
+    },
+    {
+      question: "what are the two ways to increase wealth?",
+      answer: "income and assets."
+    },
+    {
+      question: "what are the three ways to do it through income?",
+      answer: "Dividend Valuation Model (DVM), Price to Earnings (P/E), and Cashflow Model."
+    },
+    {
+      question: "what are the five Dividend calculations?",
+      answer: "constant, basic growth, year-to-year growth, present value of future dividends, and intrinsic share price."
+    },
+    {
+      question: "what is the constant dividend calculation?",
+      answer: "Dividend Paid x Required Rate of Return."
+    },
+    {
+      question: "what is the basic growth dividend calculation?",
+      answer: "Diviend Paid x (1 + Growth Rate)"
+    },
+    {
+      question: "what is the year-to-year growth dividend calculation?",
+      answer: "Dividned Paid x (1 + Growth Rate) ^ Period"
+    },
+    {
+      question: "what is the present value of future dividends calculation?",
+      answer: "Dividend Paid x (1 + Growth) ^ Period / (1 + Required Return) ^ Period"
+    },
+    {
+      question: "what is the instrinsic share price calculation?",
+      answer: "(Dividend Paid x (1 + Growth)) / (Required Return - Growth)"
+    },
     {
       question: "",
       answer: ""
@@ -142,6 +230,6 @@ const CARD_DATA = {
       question: "",
       answer: ""
     },
-  ]  
+  ]
 
 };
